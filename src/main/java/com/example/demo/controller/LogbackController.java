e9570f1fa88077f42061c128d0b264d28b9e6d76
yifei <yifei.yao@yahoo.com>
2019-04-08 22:57:43 +0800

    验证post，get方法

--- src/main/java/com/example/demo/controller/LogbackController.java
+++ src/main/java/com/example/demo/controller/LogbackController.java
@@ -3,10 +3,16 @@ package com.example.demo.controller;
 import com.example.demo.exc.MyException;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.forms.TrailForm;
+
+import javax.xml.ws.Response;
+import java.util.HashMap;
+import java.util.Map;
+
 
 @RestController
+@RequestMapping("/sentry")
 public class LogbackController {
 
     /** 方法会发生500错误
@@ -15,14 +21,55 @@ public class LogbackController {
 
     private final Logger logger = LoggerFactory.getLogger(LogbackController.class);
 
-    @GetMapping("/helloworld")
-    public String helloworld() {
+    @GetMapping("/record")
+    public String recordException(@RequestParam String name) {
 
         try{
-             throw new MyException("这是一个错误！");
+            throw new MyException("遇见"+name+"是个错误！");
         }catch (Exception msg){
             logger.error(msg.getMessage(),msg);
         }
         return "Hello World!";
+    }
+
+//  post方式，参数是json类型
+    @PostMapping("/trail1")
+    public TrailForm taril1(@RequestBody TrailForm trailForm){
+
+        return trailForm;
+    }
+
+//  post方式，参数不是json类型
+    @PostMapping("/trail4")
+    public TrailForm taril4(TrailForm trailForm){
+
+        return trailForm;
+    }
+
+//  post方式，参数不是json类型
+    @PostMapping("/trail5")
+    public String taril5(@RequestParam String name,
+                            @RequestParam String address,
+                            @RequestParam Integer age){
+
+        return "hello";
+    }
+
+
+    @GetMapping("/trail2")
+    public Map taril2(@RequestParam String name,
+                                     @RequestParam String address,
+                                     @RequestParam Integer age){
+        Map<String,String> resultMap = new HashMap<String,String>();
+        resultMap.put("name", name);
+        resultMap.put("addr", address);
+        resultMap.put("age", String.valueOf(age));
+        return resultMap;
+    }
+
+    @GetMapping("/trail3")
+    public TrailForm taril3(TrailForm trailForm){
+
+        return trailForm;
     }
 }
